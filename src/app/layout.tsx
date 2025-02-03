9e713abbfd8e3db00745b228aa58c64b897aeb38
tim-schleeh <tim@schleeh.eu>
2025-02-03 12:41:30 +0100

    feat: basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div
+          id="mainContainer"
+          className="flex sm:items-center justify-center min-h-screen"
+        >
+          <div className="flex flex-col m-2 sm:m-0 sm:h-[80vh] w-full sm:w-[80vw] sm:max-w-[800px] sm:max-h-[600px] font-[family-name:var(--font-geist-sans)] rounded-2xl border overflow-auto">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="text-white text-center p-4">Footer</footer>;
+}
